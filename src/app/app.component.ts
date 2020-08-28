31b6304796e9b1dad574226c4df46a6eeb036256
Siva Patnayak <sankarpatnayak1955@gmail.com>
2020-08-28 10:49:22 +0530

    Angular Webapp base code

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Hi Siva. This is First Angular app with Docker';
+}
